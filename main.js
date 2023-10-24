84f3bdb40b02bae02961705b469dbf97f68fc42c
Magelulw <magelulw@gmail.com>
2023-10-24 15:37:10 +0200

    added some javascript code

--- main.js
+++ main.js
@@ -17,9 +17,41 @@ function gameModule(){
     function choosePlayer(){
         const choosePlayerBtn = document.querySelectorAll(".buttonHover")
 
+        const playerHuman1 = document.getElementById("human1")
+        const playerHuman2 = document.getElementById("human2")
+        const playerBot1 = document.getElementById("bot1")
+        const playerBot2 = document.getElementById("bot2")
+
         choosePlayerBtn.forEach(btn =>{
             btn.addEventListener("click",function(){
-                btn.style.backgroundColor = "red"
+                let value = btn.value
+
+                switch(value) {
+                    case "human1":
+                        playerHuman1.classList.add("selectedChoiceBtn")
+                        playerBot1.classList.remove("selectedChoiceBtn")
+
+                        break;
+
+                    case "bot1":
+                        playerBot1.classList.add("selectedChoiceBtn")
+                        playerHuman1.classList.remove("selectedChoiceBtn")
+
+                        break;
+                        
+                    case "human2":
+                        playerHuman2.classList.add("selectedChoiceBtn")
+                        playerBot2.classList.remove("selectedChoiceBtn")
+    
+                        break;        
+
+                    case "bot2":
+                        playerBot2.classList.add("selectedChoiceBtn")
+                        playerHuman2.classList.remove("selectedChoiceBtn")
+
+                        default:
+                            return "there is something wrong"
+                }
             })
         })
 
