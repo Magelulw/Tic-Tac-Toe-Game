5be970f028209db60b6bc7c1555e69be9c2472eb
Magelulw <magelulw@gmail.com>
2023-10-25 17:57:35 +0200

    started with the game design

--- main.js
+++ main.js
@@ -15,12 +15,16 @@ function gameModule(){
     }
 
     function choosePlayer(){
+        let player1 = "";
+        let player2 = "";
+
+
         const choosePlayerBtn = document.querySelectorAll(".buttonHover")
 
-        const playerHuman1 = document.getElementById("human1")
-        const playerHuman2 = document.getElementById("human2")
-        const playerBot1 = document.getElementById("bot1")
-        const playerBot2 = document.getElementById("bot2")
+        const playerHuman1Choice = document.getElementById("human1")
+        const playerHuman2Choice = document.getElementById("human2")
+        const playerBot1Choice = document.getElementById("bot1")
+        const playerBot2Choice = document.getElementById("bot2")
 
         choosePlayerBtn.forEach(btn =>{
             btn.addEventListener("click",function(){
@@ -28,26 +32,32 @@ function gameModule(){
 
                 switch(value) {
                     case "human1":
-                        playerHuman1.classList.add("selectedChoiceBtn")
-                        playerBot1.classList.remove("selectedChoiceBtn")
+                        player1 = "human"
+                        playerHuman1Choice.classList.add("selectedChoiceBtn")
+                        playerBot1Choice.classList.remove("selectedChoiceBtn")
+                        
 
                         break;
 
                     case "bot1":
-                        playerBot1.classList.add("selectedChoiceBtn")
-                        playerHuman1.classList.remove("selectedChoiceBtn")
+                        player1 = "bot"
+                        playerBot1Choice.classList.add("selectedChoiceBtn")
+                        playerHuman1Choice.classList.remove("selectedChoiceBtn")
+                        
 
                         break;
                         
                     case "human2":
-                        playerHuman2.classList.add("selectedChoiceBtn")
-                        playerBot2.classList.remove("selectedChoiceBtn")
+                        player2 = "human"
+                        playerHuman2Choice.classList.add("selectedChoiceBtn")
+                        playerBot2Choice.classList.remove("selectedChoiceBtn")
     
                         break;        
 
                     case "bot2":
-                        playerBot2.classList.add("selectedChoiceBtn")
-                        playerHuman2.classList.remove("selectedChoiceBtn")
+                        player2 = "bot"
+                        playerBot2Choice.classList.add("selectedChoiceBtn")
+                        playerHuman2Choice.classList.remove("selectedChoiceBtn")
 
                         default:
                             return "there is something wrong"
@@ -55,7 +65,21 @@ function gameModule(){
             })
         })
 
+        chooseSign(player1,player2)
     }
+
+    function chooseSign(player1,player2){
+        let gameTurn = 0;
+        
+        switch(gameTurn){
+            case gameTurn % 2 == 0:
+
+        }
+
+
+
+    }
+
 
     
 
@@ -66,19 +90,35 @@ function gameModule(){
     
         for(let i = 1; i <= 9; i++){
             let newDiv = document.createElement("div")
-    
+            
+            newDiv.setAttribute("gameCell",i)
             newDiv.classList.add("CellSize")
             gameFieldSection.appendChild(newDiv)
     
         }
+
+        zeroSignTurn()
     }
     return{
         startGameBtn,
         choosePlayer,
-        addGameField
+        addGameField,
         
     }
 }
+
+
+function zeroSignTurn(){
+    const getNewDiv = document.querySelectorAll(".gameSection [gameCell]")
+
+    getNewDiv.forEach(div =>{
+        div.addEventListener("click",() =>{
+            div.classList.add("zeroSign")
+            div.value = 0
+        })
+    })
+}
+
 
 
 function gameInitializer(){
--- practices/test.js
+++ practices/test.js
@@ -1,17 +1,140 @@
-function createDiv(){
-    const game = document.querySelector(".game")
-    const summonDivBtn = document.querySelector(".summonDiv")
-    
-    summonDivBtn.addEventListener("click",function(){
-        for(let i = 0; i < 9; i++){
-            let newDiv = document.createElement("div")
-    
-            newDiv.classList.add("cells")
+function gameModule(){
 
-            game.appendChild(newDiv)
+    function startGameBtn(){
+        const startGameBtn = document.querySelector(".startGameBtn")
+        const selectSection = document.querySelector(".selectSection")
+        const gameSection = document.querySelector(".gameSection")
+    
+        startGameBtn.addEventListener("click", function(){
+            selectSection.classList.add("hidden")
+            gameSection.classList.remove("hidden")
+            
+
+            addGameField()
+        })
+    }
+
+    function choosePlayer(){
+        let player1 = "";
+        let player2 = "";
+
+
+        const choosePlayerBtn = document.querySelectorAll(".buttonHover")
+
+        const playerHuman1Choice = document.getElementById("human1")
+        const playerHuman2Choice = document.getElementById("human2")
+        const playerBot1Choice = document.getElementById("bot1")
+        const playerBot2Choice = document.getElementById("bot2")
+
+        choosePlayerBtn.forEach(btn =>{
+            btn.addEventListener("click",function(){
+                let value = btn.value
+
+                switch(value) {
+                    case "human1":
+                        player1 = "human"
+                        playerHuman1Choice.classList.add("selectedChoiceBtn")
+                        playerBot1Choice.classList.remove("selectedChoiceBtn")
+                        
+
+                        break;
+
+                    case "bot1":
+                        player1 = "bot"
+                        playerBot1Choice.classList.add("selectedChoiceBtn")
+                        playerHuman1Choice.classList.remove("selectedChoiceBtn")
+                        
+
+                        break;
+                        
+                    case "human2":
+                        player2 = "human"
+                        playerHuman2Choice.classList.add("selectedChoiceBtn")
+                        playerBot2Choice.classList.remove("selectedChoiceBtn")
+    
+                        break;        
+
+                    case "bot2":
+                        player2 = "bot"
+                        playerBot2Choice.classList.add("selectedChoiceBtn")
+                        playerHuman2Choice.classList.remove("selectedChoiceBtn")
+
+                        default:
+                            return "there is something wrong"
+                }
+            })
+        })
+
+        chooseSign(player1,player2)
+    }
+
+    function chooseSign(player1,player2){
+        let gameTurn = 0;
+        
+        switch(gameTurn){
+            case gameTurn % 2 == 0:
+
         }
-    })
 
+
+
+    }
+
+
+    
+
+
+    function addGameField(){
+        const gameFieldSection = document.querySelector(".gameSection");
+
+    
+        for(let i = 1; i <= 9; i++){
+            let newDiv = document.createElement("div")
+            
+            newDiv.setAttribute("gameCell",i)
+            newDiv.classList.add("CellSize")
+            gameFieldSection.appendChild(newDiv)
+    
+        }
+
+        zeroSignTurn()
+    }
+    return{
+        startGameBtn,
+        choosePlayer,
+        addGameField,
+        
+    }
 }
 
-createDiv()
+
+function zeroSignTurn(){
+    const getNewDiv = document.querySelectorAll(".gameSection [gameCell]")
+
+    getNewDiv.forEach(div =>{
+        div.addEventListener("click",() =>{
+            div.classList.add("zeroSign")
+            div.value = 0
+        })
+    })
+}
+
+
+
+function gameInitializer(){
+    const game = gameModule()
+    
+    function playTheGame(){
+        game.startGameBtn()
+        game.choosePlayer()
+    }
+
+    return {
+        playTheGame,
+
+    }
+}
+
+
+const gameInit = gameInitializer();
+gameInit.playTheGame();
